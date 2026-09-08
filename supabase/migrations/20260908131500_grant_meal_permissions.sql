bb90ce4ea1d70fdbaf84297d098c49ae441c8c2d
himanchal08 <khattrihimanchal17@gmail.com>
2026-09-08 19:02:11 +0530

    feat: grant read permissions for meal tables to anon and authenticated roles

---
+++ supabase/migrations/20260908131500_grant_meal_permissions.sql
@@ -0,0 +1,3 @@
+GRANT SELECT ON public.meal_constants TO anon, authenticated;
+GRANT SELECT ON public.meal_rotation TO anon, authenticated;
+GRANT SELECT ON public.meal_choices TO anon, authenticated;
